3f768105a1100dfa84ef9b6675d1961248967efb
srikanthgopu-321 <gopusrikanthreddy71@gmail.com>
2020-10-29 16:43:25 +0530

    added:calculate Wage Till condition reached

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,7 +3,7 @@ public class EmployeeWage {
 public static void main(String[] args) {
 		System.out.println(
 				"-------------------------Welcome to Employee Wage Computation Problem-------------------------");
-		int IS_FULL_TIME = 1;
+/*		int IS_FULL_TIME = 1;
 		int IS_PART_TIME = 2;
 		int attendance = (int) (Math.floor(Math.random() * 10) % 3);
 		System.out.println(attendance);
@@ -79,6 +79,51 @@ int workingDaysInMonth = 20;
 		System.out.println("Employee's salary for the month is " + totalMonthlyWage);
 	}
 
+*///}
+
+
+int empWagePerHour = 20;
+		int empHrs = 0;
+		int empDailyWage = 0;
+		int workingDaysInMonth = 20;
+		int totalMonthlyWage = 0;
+		int totalEmpDays = 0;
+		int totalHrs = 0;
+		int end = 0;
+		while (end >= 0) {
+			int attendance = (int) (Math.floor(Math.random() * 10) % 3);
+			System.out.println(attendance);
+			switch (attendance) {
+			case 0:
+				System.out.println("Employee is absent");
+				System.out.println();
+				empHrs = 0;
+				break;
+			case 1:
+				System.out.println("Employee is present");
+				System.out.println();
+				empHrs = 8;
+				totalEmpDays++;
+				break;
+			case 2:
+				System.out.println("Employee is present for Part time");
+				System.out.println();
+				empHrs = 4;
+				totalEmpDays++;
+				break;
+			}
+			totalHrs += empHrs;
+			empDailyWage = empWagePerHour * empHrs;
+			System.out.println("Employee's salary for the day is " + empDailyWage);
+			totalMonthlyWage += empDailyWage;
+			if (totalEmpDays == 20 || totalHrs == 100) {
+				break;
+			}
+		}
+		System.out.println("Employee's salary for the month is " + totalMonthlyWage);
+		System.out.println("Total Employee Hours:" + totalHrs + "total Employee days" + totalEmpDays);
+	}
+
 }
 
 
@@ -97,11 +142,3 @@ int workingDaysInMonth = 20;
 
 
 
-
-
-
-//	}
-
-
-
-//}
