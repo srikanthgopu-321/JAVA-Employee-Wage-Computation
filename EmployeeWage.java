35c5b30f99399117edfc94ac4dbd57dc86e012ec
srikanthgopu-321 <gopusrikanthreddy71@gmail.com>
2020-10-29 15:58:23 +0530

    added:Part Time Wage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,14 +1,11 @@
-public class EmployeeWage{
-	public static void main(String[] args){
-	System.out.println("Welcome to Employee Wage Computation");
-	int IS_FULL_TIME = 1;
-			int attendance = (int) (Math.round(Math.random()));
-			if (attendance == IS_FULL_TIME)
-				System.out.println("Employee is present");
-			else
-				System.out.println("Employee is absent");
-//	int IS_FULL_TIME = 1;
-//		int attendance = (int) (Math.round(Math.random()));
+public class EmployeeWage {
+	public static void main(String[] args) {
+		System.out.println(
+				"-------------------------Welcome to Employee Wage Computation Problem-------------------------");
+		int IS_FULL_TIME = 1;
+		int IS_PART_TIME = 2;
+		int attendance = (int) (Math.floor(Math.random() * 10) % 3);
+		System.out.println(attendance);
 		int empWagePerHour = 20;
 		int empHrs = 0;
 		int empDailyWage = 0;
@@ -16,12 +13,17 @@ public class EmployeeWage{
 			System.out.println("Employee is present");
 			System.out.println();
 			empHrs = 8;
-		} else
-		{
+		} else if (attendance == IS_PART_TIME) {
+			System.out.println("Employee is present for Part time");
+			System.out.println();
+			empHrs = 4;
+
+		} else {
 			System.out.println("Employee is absent");
-		System.out.println();
+			System.out.println();
 		}
-		empDailyWage=empWagePerHour*empHrs;
-		System.out.println("Employee's salary for the day is "+empDailyWage);
+		empDailyWage = empWagePerHour * empHrs;
+		System.out.println("Employee's salary for the day is " + empDailyWage);
 	}
+
 }
