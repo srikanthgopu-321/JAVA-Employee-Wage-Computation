7b0427b3340cbbff4fd12919e71a42edf748025b
srikanthgopu-321 <gopusrikanthreddy71@gmail.com>
2020-10-29 15:22:23 +0530

    added:Employee is present or not

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,5 +1,11 @@
 public class EmployeeWage{
 	public static void main(String[] args){
 	System.out.println("Welcome to Employee Wage Computation");
+	int IS_FULL_TIME = 1;
+			int attendance = (int) (Math.round(Math.random()));
+			if (attendance == IS_FULL_TIME)
+				System.out.println("Employee is present");
+			else
+				System.out.println("Employee is absent");
 	}
 }
