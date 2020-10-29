408715295ff04e01763985afa01b60677c3b0cb5
srikanthgopu-321 <gopusrikanthreddy71@gmail.com>
2020-10-29 16:19:21 +0530

    added:Employee-Wage-SwitchCase

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,5 +1,6 @@
-public class EmployeeWage {
-	public static void main(String[] args) {
+
+public class EmployeeWage {	
+public static void main(String[] args) {
 		System.out.println(
 				"-------------------------Welcome to Employee Wage Computation Problem-------------------------");
 		int IS_FULL_TIME = 1;
@@ -24,6 +25,30 @@ public class EmployeeWage {
 		}
 		empDailyWage = empWagePerHour * empHrs;
 		System.out.println("Employee's salary for the day is " + empDailyWage);
+
+
+
+
+	 switch (attendance) {
+		case 0:
+			System.out.println("Employee is absent");
+			System.out.println();
+			break;
+		case 1:
+			System.out.println("Employee is present");
+			System.out.println();
+			empHrs = 8;
+			break;
+		case 2:
+			System.out.println("Employee is present for Part time");
+			System.out.println();
+			empHrs = 4;
+			break;
+		}
+		empDailyWage = empWagePerHour * empHrs;
+		System.out.println("Employee's salary for the day is " + empDailyWage);
 	}
+
+
 
 }
