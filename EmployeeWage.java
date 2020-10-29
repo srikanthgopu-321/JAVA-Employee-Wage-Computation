392005dacea17025a301b419573881fad0293aa8
srikanthgopu-321 <gopusrikanthreddy71@gmail.com>
2020-10-29 16:30:15 +0530

    added:Calulation of Wage Per Month

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -47,8 +47,61 @@ public static void main(String[] args) {
 		}
 		empDailyWage = empWagePerHour * empHrs;
 		System.out.println("Employee's salary for the day is " + empDailyWage);
+
+
+
+int workingDaysInMonth = 20;
+		int totalMonthlyWage = 0;
+		for (int i = 0; i < workingDaysInMonth; i++) {
+//			int attendance = (int) (Math.floor(Math.random() * 10) % 3);
+			System.out.println(attendance);
+			switch (attendance) {
+			case 0:
+				System.out.println("Employee is absent");
+				System.out.println();
+				empHrs = 0;
+				break;
+			case 1:
+				System.out.println("Employee is present");
+				System.out.println();
+				empHrs = 8;
+				break;
+			case 2:
+				System.out.println("Employee is present for Part time");
+				System.out.println();
+				empHrs = 4;
+				break;
+			}
+			empDailyWage = empWagePerHour * empHrs;
+			System.out.println("Employee's salary for the day is " + empDailyWage);
+			totalMonthlyWage += empDailyWage;
+		}
+		System.out.println("Employee's salary for the month is " + totalMonthlyWage);
 	}
 
-
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//	}
+
+
+
+//}
